59f3b54d7ec1ddc808a0afd70020feb19ada4e6a
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-09-11 21:28:19 +0300

    feat: implement feature according to the task#2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  var num = 0;
+  if (preferences.length > 2) {
+    for (var i = 0; i < preferences.length; i++) {
+      num = preferences[i];
+      if (num == (i + 1)) continue;
+      num = preferences[preferences[num - 1] - 1];
+      if (num == (i + 1)) {
+        count++;
+      }
+    }
+    return count/3; 
+  } else {
+    return count;
+  }
 };
